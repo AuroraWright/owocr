fb6b55ae6d21fd3df679ccaa93f691c4888f073d
AuroraWright <AuroraWright@users.noreply.github.com>
2024-01-24 17:23:33 +0100

    Increase Lens timeout

--- owocr/ocr.py
+++ owocr/ocr.py
@@ -171,7 +171,7 @@ class GoogleLens:
         url = f'https://lens.google.com/v3/upload?stcs={timestamp}'
         files = {'encoded_image': ('owo' + str(timestamp) + '.png', self._preprocess(img), 'image/png')}
         try:
-            res = requests.post(url, files=files, timeout=(3, 10))
+            res = requests.post(url, files=files, timeout=30)
         except requests.exceptions.Timeout:
             return 'Request timeout!'
 
