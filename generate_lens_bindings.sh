c7634a47036d834e65bd4a783bd5b81d388ad4d6
AuroraWright <AuroraWright@users.noreply.github.com>
2025-12-09 08:14:22 +0100

    Update script

--- generate_lens_bindings.sh
+++ generate_lens_bindings.sh
@@ -7,6 +7,6 @@ fi
 
 PROTO_PATH="$1"
 
-mkdir -p owocr/py_lens
-protoc "--proto_path=$PROTO_PATH" --python_out=owocr/py_lens $PROTO_PATH/*.proto
-sed -i '' "s/import \(.*_pb2\) as \(.*\)/from . import \1 as \2/" owocr/py_lens/*
+mkdir -p owocr/lens_protos
+protoc "--proto_path=$PROTO_PATH" --python_out=owocr/lens_protos $PROTO_PATH/*.proto
+sed -i '' "s/import \(.*_pb2\) as \(.*\)/from . import \1 as \2/" owocr/lens_protos/*
