48bcdfc0afed39aec792428c8b36e81733be97b1
AuroraWright <AuroraWright@users.noreply.github.com>
2026-02-03 23:18:20 +0100

    No need for this

--- owocr/__main__.py
+++ owocr/__main__.py
@@ -6,10 +6,7 @@ def main():
     multiprocessing.set_start_method('spawn')
     if sys.platform == 'darwin':
         os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
-    try:
-        from .run import run
-    except ImportError:
-        from owocr.run import run
+    from owocr.run import run
     run()
 
 if __name__ == '__main__':
